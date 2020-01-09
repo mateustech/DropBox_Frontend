15512bf496dcf2838ba3df8bd1cdaa5f832845a0
mateustech <mateustech686@gmail.com>
2020-01-08 22:36:11 -0300

    Adicionando Axios

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, { useState, setState } from 'react';
+import React, { useState } from 'react';
 import api from '../../service/api'
 import './styles.css';
 import logo from '../../assents/logo.png'
